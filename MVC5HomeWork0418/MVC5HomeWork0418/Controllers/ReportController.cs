4ed68fea2459d4e61cb29191fa7fd907aef81e73
alexch_cheng <alexch_cheng@compal.com>
2015-04-25 07:12:04 +0800

    將bin、obj資料夾和*.user檔案加入ignore清單 新增ReportController
    Signed-off-by: alexch_cheng <alexch_cheng@compal.com>

---
+++ MVC5HomeWork0418/MVC5HomeWork0418/Controllers/ReportController.cs
@@ -0,0 +1,92 @@
+﻿using MVC5HomeWork0418.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5HomeWork0418.Controllers
+{
+    public class ReportController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View(db.客戶報表.ToList());
+        }
+
+        // GET: Report/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Report/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Report/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Report/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Report/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Report/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Report/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
